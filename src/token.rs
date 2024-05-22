10e77e9b5ec6bea2014d558ec28993afe71fe51b
AvaterClasher <avater.clasher@gmail.com>
2024-05-22 11:29:43 +0530

    feat(token): :sparkles: implemented the token enum

---
+++ src/token.rs
@@ -0,0 +1,54 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Token {
+    Eof,
+    Illegal,
+
+    // Literals
+    Ident(String),
+    Int(i32),
+    String(String),
+    Boolean(bool),
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    Percent,
+
+    // Comparison
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LessEqual,
+    GreaterEqual,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+
+    // Keywords
+    Let,
+    Fn,
+    If,
+    Else,
+    Return,
+    Import,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
