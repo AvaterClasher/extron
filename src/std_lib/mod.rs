595df53d6176ed15fd8f84b5ee2ffeb7f9d6185e
AvaterClasher <avater.clasher@gmail.com>
2024-05-25 16:37:06 +0530

    feat(std): :sparkles: Added math and string libs

--- src/std_lib/mod.rs
+++ src/std_lib/mod.rs
@@ -2,9 +2,11 @@ use std::collections::HashMap;
 
 use crate::eval::object::Object;
 
-pub mod util;
 pub mod array;
 pub mod fs;
+pub mod math;
+pub mod string;
+pub mod util;
 
 pub struct Res {
     pub globals: HashMap<String, Object>,
@@ -15,6 +17,8 @@ pub fn get_std_lib(lib: String) -> Option<Res> {
         "std:util" => Some(util::add_globals()),
         "std:array" => Some(array::add_globals()),
         "std:fs" => Some(fs::add_globals()),
+        "std:string" => Some(string::add_globals()),
+        "std:math" => Some(math::add_globals()),
         _ => None,
     }
 }
