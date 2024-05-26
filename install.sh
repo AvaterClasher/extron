309a868fc949a637d8f6b202f46f3d5d31e67337
AvaterClasher <avater.clasher@gmail.com>
2024-05-26 12:58:29 +0530

    feat(script): :fire: removed if statement becuase wsl not supported

--- install.sh
+++ install.sh
@@ -7,11 +7,7 @@ LINUX_URL="https://github.com/AvaterClasher/extron/releases/download/latest/extr
 TARGET_DIR=$(echo $PATH | tr ':' '\n' | grep -vE '^$' | head -n 1)
 
 # Determine the OS and download the appropriate executable
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    EXE_PATH="$TARGET_DIR/extron"
-    curl -L $LINUX_URL -o $EXE_PATH
-    chmod +x $EXE_PATH
-    echo "Downloaded and placed the executable at $EXE_PATH"
-else
-    echo "Unsupported OS."
-fi
+EXE_PATH="$TARGET_DIR/extron"
+curl -L $LINUX_URL -o $EXE_PATH
+chmod +x $EXE_PATH
+echo "Downloaded and placed the executable at $EXE_PATH"
